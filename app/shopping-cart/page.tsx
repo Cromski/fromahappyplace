6077f20d1031f12c61de3026bcbb32585a430f51
Jakob Rasmus Henriksen <jarh@itu.dk>
2025-04-19 01:02:46 +0200

    start på cart page

---
+++ app/shopping-cart/page.tsx
@@ -0,0 +1,17 @@
+"use client"
+import { useUserStore } from "../stores/userStore"
+
+
+const ShoppingCart = () => {
+    const user = useUserStore((state) => state.userData)
+
+    return (
+        <div>
+            {user?.cart.map((item) => (
+                <div key={item.id}>{item.data.clothingId}</div>
+            ))}
+        </div>
+    )
+}
+
+export default ShoppingCart
