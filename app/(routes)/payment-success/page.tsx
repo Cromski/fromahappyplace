3ea0512f02b37290e500e73aad4c335929c8e778
Cromski <jakobrhenriksen@outlook.com>
2025-06-15 00:07:23 +0200

    Update page.tsx

--- app/(routes)/payment-success/page.tsx
+++ app/(routes)/payment-success/page.tsx
@@ -8,8 +8,7 @@ interface PaymentSuccessProps {
     const { amount } = await searchParams;
   
     return (
-      <main className="max-w-6xl mx-auto p-10 text-white text-center border m-10 rounded-md bg-gradient-to-tr from-blue-500 to-purple-500">
-        <div className="mb-10">
+      <div className="mb-10">
           <h1 className="text-4xl font-extrabold mb-2">Thank you!</h1>
           <h2 className="text-2xl">You successfully sent</h2>
   
@@ -17,7 +16,6 @@ interface PaymentSuccessProps {
             DKK {amount}
           </div>
         </div>
-      </main>
     );
   }
 
