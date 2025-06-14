a75eb2aab875890db78a8d9bfc46d4f962d9c206
Cromski <jakobrhenriksen@outlook.com>
2025-06-14 23:04:29 +0200

    Update page.tsx

--- app/(routes)/payment/page.tsx
+++ app/(routes)/payment/page.tsx
@@ -8,13 +8,8 @@ import convertToSubcurrency from "@lib/convertToSubcurrency";
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 
-if (process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY === undefined){
-    throw new Error("NEXT_PUBLIC_STRIPE_PUBLIC_KEY is not defined")
-}
-
-const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY)
-
 const PaymentPage = () => {
+    const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY!)
     const user = useUserStore((state) => state.userData)
     const [pieceInfo, setPieceInfo] = useState<ClothingItem | null>(null)
     const [totalAmount, setTotalAmount] = useState(0)
